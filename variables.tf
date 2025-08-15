98504dd70b16b9f6b8f1db4d3ff441f11687fc16
Andrii Sydorka <y804356@syniverse.com>
2025-08-15 18:24:50 +0300

    ``

--- variables.tf
+++ variables.tf
@@ -1,31 +1,20 @@
 variable "api_url" {
   description = "URL to the API of Proxmox"
   type        = string
-  default     = "https://192.168.1.101:8006/api2/json"
+  default     = "https://192.168.50.134:8006/api2/json"
 }
 
-variable "user" {
-  description = "Name of the admin account to use"
-  type        = string
-  default     = "terraform-prov@pve"
-}
 
-variable "passwd" {
-  description = "Password for the Terraform user - defined in the file terraform.tfvars"
+variable "token" {
+  description = "token for auth"
   type        = string
-  sensitive   = true
+  default = "apiuser@pve!mytoken=a40facf6-0251-46e8-bfac-a63ef3181b4c"
 }
 
 variable "target_host" {
   description = "Hostname to deploy to"
   default     = "dantooine"
   type        = string
-}
-
-variable "lxc_passwd" {
-  description = "Password for the root user on containers"
-  type        = string
-  sensitive   = true
 }
 
 variable "storage_name" {
@@ -49,11 +38,11 @@ variable "pool" {
 variable "ansible_password" {
   type        = string
   description = "Password for the ansible user"
-  sensitive   = true
+  default     = "test"
 }
 variable "ansible_public_ssh_key" {
   type        = string
   description = "Private SSH key for the user ansible"
-  sensitive   = true
+  default     = "test"
 
 }
