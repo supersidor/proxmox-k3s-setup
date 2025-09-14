8b058968a20311587d2e49130e88105230cf46c3
supersidor <y804356@syniverse.com>
2025-09-14 11:25:50 +0300

    1

--- scripts/init.sh
+++ scripts/init.sh
@@ -1,3 +1,15 @@
 #!/bin/zsh
 
-ansible-playbook -vv -i inventory/hosts.ini setup-k3s.yaml -u test --private-key ~/.ssh/id_rsa
+# Base ansible-playbook command
+ANSIBLE_CMD="ansible-playbook -vv -i inventory/hosts.ini setup-k3s.yaml -u test --private-key ~/.ssh/id_rsa"
+
+# Check if limit parameter is provided
+if [ -n "$1" ]; then
+    echo "Running playbook with limit: $1"
+    ANSIBLE_CMD="$ANSIBLE_CMD --limit $1"
+else
+    echo "Running playbook for all hosts"
+fi
+
+# Execute the command
+eval $ANSIBLE_CMD
