79060a2948fa95ad06ec3f30fcec1a7f0f2edd36
supersidor <sidorka@gmail.com>
2025-09-14 11:25:50 +0300

    1

--- scripts/revert.sh
+++ scripts/revert.sh
@@ -1,3 +1,11 @@
 #!/bin/zsh
 
-ansible-playbook revert-snapshot.yaml -e "proxmox_password=Hello123" -e "snapshot_name=start"
+# Check if snapshot name parameter is provided
+if [ -z "$1" ]; then
+    echo "Usage: $0 <snapshot_name>"
+    echo "Example: $0 start"
+    exit 1
+fi
+
+echo "Reverting to snapshot: $1"
+ansible-playbook revert-snapshot.yaml -e "proxmox_password=Hello123" -e "snapshot_name=$1"
